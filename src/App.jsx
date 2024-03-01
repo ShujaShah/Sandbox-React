30e7132a6fb2a1bd964062b8fe05c8e20e414cc1
Shuja <shuja.ashraf11@gmail.com>
2024-03-01 19:41:06 +0000

    created a simple app

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,33 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 export default function App() {
   const [advice, setAdvice] = useState("");
+  const [count, setCount] = useState(0);
+
   async function getAdvice() {
     const res = await fetch("https://api.adviceslip.com/advice");
     const data = await res.json();
     setAdvice(data.slip.advice);
+    setCount(count + 1);
   }
+  //useEffect is used to run the function everytime the component gets loaded first
+  useEffect(() => {
+    getAdvice();
+  }, []);
   return (
     <div>
-      <h1>Hello World!</h1>
+      <h2>{advice} </h2>
       <button onClick={getAdvice}>Get Advice</button>
-      <p>{advice} </p>
+      <Message count={count} />
+    </div>
+  );
+}
+
+function Message(props) {
+  return (
+    <div>
+      <p>
+        You have read <strong>{props.count}</strong> pieces of advice
+      </p>
     </div>
   );
 }
