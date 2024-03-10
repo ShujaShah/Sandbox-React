e9819f24443871e3a1ecd1fab8a1046840eed23f
Shuja <shuja.ashraf11@gmail.com>
2024-03-10 16:59:42 +0000

    Implemented state

--- src/components/Steps.jsx
+++ src/components/Steps.jsx
@@ -1,7 +1,17 @@
+import { useState } from "react";
 import messages from "../../public/messages";
 
 const Steps = () => {
-  const step = 1;
+  console.log("here are the messages", messages);
+  //const step = 2;
+  const [step, setStep] = useState(1);
+
+  function handleNext() {
+    setStep(step + 1);
+  }
+  function handlePrevious() {
+    setStep(step - 1);
+  }
   return (
     <div className="steps">
       <div className="numbers">
@@ -10,13 +20,19 @@ const Steps = () => {
         <div className={`${step >= 3 ? "active" : ""}`}>3</div>
       </div>
       <p className="message">
-        Step{step} : {messages[step - 1]}
+        Step {step} : {messages[step - 1]}
       </p>
       <div className="buttons">
-        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={handlePrevious}
+        >
           Previous
         </button>
-        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={handleNext}
+        >
           Next
         </button>
       </div>
